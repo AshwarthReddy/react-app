2369255108e7dd3a0d5f1ff7f151872a1a54ca0e
Aswarth Reddy <aswartha.reddy_xeb@idfcbank.com>
2022-01-03 17:27:20 +0530

    1. git ignore file Updated. 2. Conditional Rendering Feature.

--- src/index.js
+++ src/index.js
@@ -3,10 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import ConditionalRendering from "./components/ConditionalRendering";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    {/*<App />*/}
+      <ConditionalRendering />
   </React.StrictMode>,
   document.getElementById('root')
 );
